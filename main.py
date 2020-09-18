9b959e4e615b8353ac5f9234d66928f3e8a31d32
Alex Weeden <tsuits@pop-os.localdomain>
2020-09-17 22:21:35 -0500

    ui

---
+++ main.py
@@ -0,0 +1,20 @@
+import PySimpleGUI as sg
+
+
+print("Hello World!")
+
+##sg.Window(title="Hello World", layout=[[]], margins=(100,50)).read()
+
+layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
+
+#Create the window
+window = sg.Window("Demo", layout)
+
+#Create an event loop
+while True:
+    event, values = window.read()
+    #End if user closes window or presses OK
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close
